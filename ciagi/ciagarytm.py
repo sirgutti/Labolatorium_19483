7caa28cbe5a80483dfd9ce2c4f90247af9508ffe
sirgutti <guttihs@gmail.com>
2022-06-23 13:19:35 +0200

    wyswietlenie

---
+++ ciagi/ciagarytm.py
@@ -0,0 +1,5 @@
+def wzory(a1, n, r):
+    an = a1 + (n - 1) * r
+    print("Ciag dla ", n, " wyrazu przyjmuej wartosc ", an)
+    sn = (a1 + an) / 2 * n
+    print("A suma wynsoi ", sn)
---
+++ ciagi/ciaggeometryczny.py
@@ -0,0 +1,5 @@
+def wzory (a1,n,q):
+    n=n-1
+    an=a1*q**n
+    n=n+1
+    print("Wyraz dla ",n," wyrazu w ciagu geometrycznym wynosi ",an)
